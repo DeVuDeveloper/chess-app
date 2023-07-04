02a68415b6690acc8d32a45bb992c224b1b3bca6
DeVuDeveloper <dejanvu.developer@gmail.com>
2023-07-04 19:34:06 +0200

    fixed stimulus and add reset controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,11 +4,8 @@
 
 import { application } from "./application"
 
-import AutoScrollController from "./auto_scroll_controller"
-application.register("autoscroll", AutoScrollController)
-
-import MessagesController from "./messages_controller"
-application.register("messages", MessagesController)
-
 import RemovalsController from "./removals_controller"
 application.register("removals", RemovalsController)
+
+import ResetFormController from "./reset_form_controller"
+application.register("reset-form", ResetFormController)
