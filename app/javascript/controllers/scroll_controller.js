3aa7779de3d54d1218737cfc085da28f270bd757
DeVuDeveloper <dejanvu.developer@gmail.com>
2023-07-05 22:44:16 +0200

    autoscroll is working using stimulus

--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,28 +1,33 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["messages"];
-
   connect() {
-    console.log("Connected to Stimulus controller.");
+    console.log("Connected to Scroll controller.");
     this.scrollToBottom();
-  }
-
-  appendMessage(event) {
-    console.log("Append message event fired.");
-    const message = event.detail.templateElement.content.firstElementChild;
-    this.messagesTarget.appendChild(message);
-    this.scrollToBottom();
-    console.log("Message appended to DOM.");
   }
 
   scrollToBottom() {
-    console.log("Scrolling to bottom.");
-    if (this.hasMessagesTarget) {
-      this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
-    }
+    const chatMessagesContainer = this.element;
+    console.log('chatMessagesContainer:', chatMessagesContainer);
+    console.log('scrollHeight:', chatMessagesContainer.scrollHeight);
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+      for (let mutation of mutationsList) {
+        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+          chatMessagesContainer.scrollTop = chatMessagesContainer.scrollHeight;
+          console.log("New scroll position:", chatMessagesContainer.scrollTop);
+        }
+      }
+    });
+
+    observer.observe(chatMessagesContainer, { childList: true });
   }
 }
+
+
+
+
+
 
 
 
